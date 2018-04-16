9b95ee967a2fcb162196e15f2665b863be5331c3
Kashfa <cyan82@hotmail.com>
2018-04-16 09:46:22 +0100

    added extention stuff

--- hero.js
+++ hero.js
@@ -1,11 +1,23 @@
-const Hero = function(name, health, fav_food, SuperPhrase) {
+const Hero = function(name, health, fav_food, SuperPhrase, task) {
   this.name = name;
   this.health = health;
   this.fav_food = fav_food
   this.SuperPhrase = SuperPhrase
+  this.task = task
 };
 
+Hero.prototype.canEatFood = function(food){
+  if (food.name === this.fav_food)
+  {
+    this.health += (food.rep_value * 1.5);
+  }
+  else
+  {
+    this.health += food.rep_value;
+  }
+  };
 
+  Hero.prototype.sortTasksByDifficultyLevel = function(task)
 
 
 module.exports = Hero;
--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -1,12 +1,19 @@
 const assert = require('assert');
 const Hero = require('../hero.js')
+const Food = require('../food.js')
+const Task = require('../task.js')
 
 describe('hero', function() {
 
   let hero;
+  let kiwi;
+  let task;
 
   beforeEach(function() {
     hero = new Hero('Super Kash', 100, 'Mac and Cheese', 'Super Kash to the rescue')
+    kiwi = new Food('Kiwi', 10)
+    task = new Task('hard')
+
   });
 
   it('should have a name', function() {
@@ -28,4 +35,11 @@ describe('hero', function() {
     const actual = hero.SuperPhrase;
     assert.strictEqual(actual, 'Super Kash to the rescue');
   });
+
+  it('food should replenishes health value', function(){
+  hero.canEatFood(kiwi)
+  actual = hero.health;
+  assert.strictEqual(actual, 110)
+ });
+ 
 });
